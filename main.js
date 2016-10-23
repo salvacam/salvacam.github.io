f916c51ba54e305a4151110add44d775a28ee178
salvacam <salvacams@gmail.com>
2016-10-23 16:40:49 +0200

    add content

---
+++ main.js
@@ -0,0 +1,510 @@
+document.onreadystatechange = function (event) {
+  if (document.readyState === 'complete') { 
+
+    var originalTitle = document.title;
+
+    var capitalizeFirstLetter = function(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    /*    Event menu button     */
+    var navButton = document.querySelector('.navbar-header button');
+    var navbar = document.querySelector('#navbar');
+    navButton.addEventListener('click', function() {
+      if (navbar.classList) {
+        navbar.classList.toggle("show");
+      } else {
+        var classes = navbar.className.split(' ');
+        var existingIndex = classes.indexOf("show");
+
+        if (existingIndex >= 0)
+          classes.splice(existingIndex, 1);
+        else
+          classes.push("show");
+
+        navbar.className = classes.join(' ');
+      }
+    });
+
+    
+    /*    Event "cursos" button     */
+    var alternarCursos = document.querySelector('#alternar-cursos');
+    var cursos = document.querySelector('#cursos');
+    alternarCursos.addEventListener('click', function(e) {
+      console.log("click");
+      e.preventDefault();
+      if (alternarCursos.textContent == 'ver') {
+        alternarCursos.textContent ='ocultar';
+        addClass(cursos, "selected");
+      } else {
+        alternarCursos.textContent = 'ver';
+        cursos.classList.remove('selected');
+      }
+    });
+
+		/***********************************************************/
+		/*******                 ROUTE                    **********/
+		/***********************************************************/
+
+
+		var elementsNav = document.querySelectorAll('.router');
+
+    /**
+    * Add class 'selected' for div and link with id-content and id 
+    * and remove class 'selected' rest of div and link
+    * @param {string} id - Identificator of tags element.
+    */
+    var selectNav = function(id) {
+      // Removed class selected for all element, link and div
+      Array.prototype.forEach.call(elementsNav, function(el, i) {
+  		  el.classList.remove('selected');
+      });
+
+      // Add class selected for link
+      var linkSelect = document.querySelector('#'+id);
+      addClass(linkSelect, "selected");
+      
+      // Add class selected for div
+      var viewSelect = document.querySelector('#'+id +"-content");
+      addClass(viewSelect, "selected");
+
+      //Change title of page, add the id, format capitaliza, to the original title 
+      document.title = originalTitle + " - " + capitalizeFirstLetter(id);
+    }
+
+    /**
+    * Add class nameClass to tag element
+    * @param {object} element - The tag element.
+    * @param {string} nameClass - The name of class.
+    */
+    var addClass = function(element, nameClass) {
+    	if (element.classList) {
+    		element.classList.add(nameClass);
+    	} else {
+    		element.nameClass += ' ' + nameClass;
+    	}
+    }
+  
+    // Removed #path from href
+    if (window.location.href.indexOf('#') > 0) {    
+    	var path = window.location.href.substring(window.location.href.indexOf('#')+1);
+    	window.location.hash = "";
+    }
+
+    // Select div and link initial
+    selectNav("inicio");    
+
+    // Add event for click in link
+    var elementsNavLink = document.querySelectorAll('a.router');
+    Array.prototype.forEach.call(elementsNavLink, function(el, i) {
+    	el.addEventListener('click', function() {
+        // Hide link when width 600px or less
+        document.getElementById("navbar").classList.remove('show');
+        selectNav(this.id);
+      });
+    });
+
+
+
+  
+    /***********************************************************/
+    /*******                 DATA                     **********/
+    /***********************************************************/
+
+  var trabajos = [
+    {
+      "empresa": 'Fidesol',   
+      "empleo": 'Desarrollador Web Front-End.',
+      "descripcion": "Desarrollador front-end, para reingenieria de proyecto con el framework Backbone.js.",
+      "lugar": "Granada",
+      "fecha": "Julio 2016 – Actualmente"
+    },
+    {
+      "empresa": 'Intelligenia',
+      "url": 'http://salvacam.x10.mx/titulos/recomendacion.pdf',
+      "empleo": 'Desarrollador Web Back-End y Front-End.',
+      "descripcion": "Desarrollo para varios proyectos, como programador front-end, backend (PHP) y gestión de bases de datos MySQL y SQL SERVER.",
+      "lugar": "Granada",
+      "fecha": "Junio 2015 – Junio 2016"
+    },
+    {
+      "empresa": 'NanoBytes Informática y Telecomunicaciones',
+      "empleo": 'Realización de prácticas de Desarrollo de Aplicaciones Web.',
+      "descripcion": "Maquetación e implementación del front-end de aplicación CloudWeb. Maquetación de web de la empresa.",
+      "lugar": "Granada",
+      "fecha": "Abril 2015 – Junio 2015"
+    },
+    {
+      "empresa": 'Soluciones y Sistemas Informáticos',
+      "empleo": 'Realización de prácticas de Administración de Sistemas Informáticos en Red.',
+      "descripcion": "Diseño e implementación de tienda web. Reparación de equipos informáticos e instalaciones de red. Creación de aplicación web para gestión de contraseñas.",
+      "lugar": "Armilla (Granada)",
+      "fecha": "Abril 2013 – Junio 2013"
+    },
+    {
+      "empresa": 'Estudio de Arquitectura Ignacio de Teresa',
+      "empleo": 'Delineante.',
+      "descripcion": "Delineación de proyectos de diferentes dimensiones de edificación y urbanismo.",
+      "lugar": "Granada",
+      "fecha": "Octubre 2007 – Junio 2014"
+    }
+  ];
+
+  var formacion = [
+    {
+      'nombre': 'Técnico Superior en Desarrollo de Aplicaciones Web',
+      'entidad': 'I.E.S. Zaidín-Vergeles, Granada.',
+      'matricula': true,
+      'fecha' : 'Junio 2015'
+    },
+    {
+      'nombre': 'Técnico Superior en Administración de Sistemas Informáticos en Red',
+      'entidad': 'I.E.S. Zaidín-Vergeles, Granada.',
+      'matricula' : true,
+      'fecha' : 'Junio 2013'
+    },
+    {
+      'nombre': 'Técnico superior en Desarrollo y Aplicación de Proyectos de Construcción',
+      'entidad': 'Complejo Educativo “SAFA-SAN LUIS”. El Pto Sta María (Cádiz)',
+      'fecha' : 'Junio 2006'
+    }
+  ];
+
+  var cursos = [
+      {
+        'nombre': 'Anatomy of Backbone.js',
+        'url': 'https://www.codeschool.com/users/1221030',
+        'entidad': 'Code School',
+        'fecha': 'Agosto 2016'
+      },
+      {
+        'nombre': 'Introducción a la metodología ágil Kanban',
+        'url': 'http://salvacam.x10.mx/titulos/kanban.pdf',
+        'entidad': 'Intelligenia',
+        'fecha': 'Junio 2016'
+      },
+      {
+        'nombre': 'Writing for the Web',
+        'url': 'http://salvacam.x10.mx/titulos/Subject_Certificate_22_May_2016.pdf',
+        'entidad': 'Open2Study',
+        'fecha': 'Mayo 2016'
+      },
+      { 
+        'nombre': 'Probar Django | Construir una Aplicación Web en Python',
+        'url': 'https://www.udemy.com/certificate/UC-C3YPRKDR',
+        'entidad': 'Udemy',
+        'fecha': 'Mayo 2016'
+      },
+      {
+        'nombre': 'User Experience for the Web',
+        'url': 'http://salvacam.x10.mx/titulos/Subject_Certificate_08_May_2016.pdf',
+        'entidad': 'Open2Study',
+        'fecha': 'Mayo 2016'
+      },
+      {
+        'nombre': 'Agilidad y Lean. Gestionando los proyectos y negocios del s. XXI (5ª. edición)',
+        'url': 'https://miriadax.net/files/10132/badge/1ff5b76e-abaa-4e36-8845-bcddff5a7d70.pdf',
+        'entidad': 'Miríada X',
+        'fecha': 'Abril 2016'
+      },
+      {
+        'nombre': 'AngularJS - Desde Hola Mundo hasta una Aplicación',
+        'url': 'https://www.udemy.com/certificate/UC-JLL09Q2W/',
+        'entidad': 'Udemy',
+        'fecha': 'Enero 2016'
+      },
+      {
+        'nombre': 'SQL com MySQL Avanzado',
+        'url': 'http://salvacam.x10.mx/titulos/aulaMento_sql2.pdf',
+        'entidad': 'Aula Mentor',
+        'fecha': 'Diciembre 2015'
+      },
+      {
+        'nombre': 'Angular.js Desde Cero, Aprende Creando una APP Web',
+        'url': 'https://www.udemy.com/certificate/UC-B685FCU8/',
+        'entidad': 'Udemy',
+        'fecha': 'Noviembre 2015'
+      },
+      {
+        'nombre': 'Desarrollo de servicios en la nube con HTML5, Javascript y node.js',
+        'url': 'https://miriadax.net/files/10132/badge/59aa6093-1e9d-48fe-bd1d-f9c2e66daf74.pdf',
+        'entidad': 'Miríada X',
+        'fecha': 'Junio 2015'
+      },
+      {
+        'nombre': 'Creación y retoque de imágenes con software libre',
+        'url': 'https://miriadax.net/files/10132/badge/799dc409-a130-46a7-98cf-a06bd658467d.pdf',
+        'entidad': 'Miríada X',
+        'fecha': 'Octubre 2014'
+      },
+      {
+        'nombre': 'Android: Introducción a la programación',
+        'url': 'http://upvx.upv.es/credenciales/21VrUqTp7221.pdf',
+        'entidad': 'UPV[X]',
+        'fecha': 'Mayo 2014'
+      },
+      {
+        'nombre': 'Desarrollo de Aplicaciones en HTML5 y para Dispositivos Móviles Firefox O.S.',
+        'url': 'https://miriadax.net/files/10132/badge/3f49030d-6319-4c85-b5e8-0f25c49af4b0.pdf',
+        'entidad': 'Miríada X',
+        'fecha': 'Diciembre 2013'
+      },
+      {
+        'nombre': 'CCNA Exploration: Routing Protocols and Concepts',
+        'entidad': 'Cisco',
+        'fecha': 'Junio 2012'
+      },
+      {
+        'nombre': 'CCNA Exploration: Network Fundamentals',
+        'entidad': 'Cisco',
+        'fecha': 'Junio 2011'
+      },
+    ];
+
+
+  var proyectos = [
+    {
+      'nombre': 'Todo List con ANGULARJS',
+      'url': 'http://salvacam.x10.mx/todo_list/',
+      'alt': 'Todo List con ANGULARJS',
+      'img': 'todo_list',
+      'git': 'https://github.com/salvacam/todo_list'
+    },
+    {
+      'nombre': 'Reproductor en html5 de Goear.com',
+      'url': 'http://salvacam.x10.mx/goear/',
+      'alt': 'Reproductor en html5 de Goear',
+      'img': 'goear',
+      'git': 'https://github.com/salvacam/goear-player'
+    },
+    {
+      'nombre': 'Quiz en NodeJS sobre Express',
+      'url': 'http://quiz-salva.herokuapp.com',
+      'alt': 'Quiz en NodeJS',
+      'img': 'quiz',
+      'git': 'https://github.com/salvacam/quiz'
+    },
+    {
+      'nombre': 'Aplicación web en tiempo real',
+      'url': 'http://localizacionjs.herokuapp.com',
+      'alt': 'Aplicación en tiempo real',
+      'img': 'localizacion',
+      'git': 'https://github.com/salvacam/localizacionjs',
+      'doc': 'http://salvacam.x10.mx/pi/Doc_.pdf',
+      'slide': 'http://slides.com/salvacam/localizacionjs/',
+    },
+    {
+      'nombre': 'Cesta de la compra de un tienda de deportes',
+      'url': 'http://salvacam.x10.mx/practica5',
+      'alt': 'Tienda on line',
+      'img': 'tienda',
+      'git': 'https://github.com/salvacam/Practica-5',
+      'doc': 'http://salvacam.x10.mx/practica5/doc.pdf',
+    },
+    {
+      'nombre': 'Web Paracaidismo',
+      'url': 'http://salvacam.x10.mx/web/practica9',
+      'alt': 'Deportes extremos',
+      'img': 'deportes',
+    },
+    {
+      'nombre': 'Juego "Hundir la flota" en JavaScript',
+      'url': 'http://salvacam.x10.mx/web/js/barquitos',
+      'alt': 'Hundir la flota en JavaScript',
+      'img': 'barquitos',
+      'git': 'https://github.com/salvacam/Barquitos'
+    },
+    {
+      'nombre': 'Podometro en HTML5',
+      'url': 'http://salvacam.x10.mx/web/js/podometro',
+      'alt': 'Podometro en HTML5',
+      'img': 'podometro',
+      'git': 'https://github.com/salvacam/Podometro'
+    },
+    {
+      'nombre': 'Web Restaurante Indio Yantra',
+      'url': 'http://salvacam.x10.mx/web/practica8',
+      'alt': 'Restaurante',
+      'img': 'restaurante',
+    },
+    {
+      'nombre': 'Gestión de una carta de restaurante mediante Ajax',
+      'url': 'http://salvacam.x10.mx/practica4',
+      'alt': 'Carta restaurante',
+      'img': 'carta',
+      'git': 'https://github.com/salvacam/Practica-5',
+      'doc': 'http://salvacam.x10.mx/practica5/doc.pdf',
+    },
+    {
+      'nombre': 'Web turística sobre Egipto',
+      'url': 'http://salvacam.x10.mx/web/practica7',
+      'alt': 'Egipto',
+      'img': 'egipto',
+    },
+    {
+      'nombre': 'Web empresa de transporte',
+      'url': 'http://salvacam.x10.mx/web/practica6',
+      'alt': 'empresa de transporte',
+      'img': 'transporte',
+    },
+    {
+      'nombre': 'Aplicación web de venta de inmuebles',
+      'url': 'http://salvacam.x10.mx/practica2',
+      'alt': 'Inmobiliaria',
+      'img': 'inmobiliaria',
+      'git': 'https://github.com/salvacam/Practica-2',
+      'doc': 'http://salvacam.x10.mx/practica2/doc.pdf',
+    }
+  ];
+
+    /***********************************************************/
+    /*******                 TEMPLATE                 **********/
+    /***********************************************************/
+
+
+    var replaceText = function(content, key, value) {
+      return content.replace("{{"+ key +"}}", value ? value : "");
+    }
+
+    var replaceTemplate = function(template, object) {
+      for(var key in object) {
+        template = replaceText(template, key, object[key]);
+      }
+      return template;      
+    }
+
+    var ifSimple = function(template, object, key) {
+      if(object[key]) {
+        var startPos = template.indexOf("{%if "+key);
+        var endPos = template.indexOf("%}", startPos);
+        var borrar = template.substring(startPos, endPos + 2);
+        template = template.replace(borrar, "");
+
+        template = replaceText(template, key, object[key]);
+        template = template.replace("{%endif%}", "");
+      } else {
+
+        var startPos = template.indexOf("{%if");
+        var endPos = template.indexOf("{%endif%}");
+        var borrar = template.substring(startPos, endPos + 9);
+        template = template.replace(borrar, "");
+      }
+      return template;
+    }
+
+    var ifComplete = function(template, object, key) {
+      if(object[key]) {
+        var startPos = template.indexOf("{%if "+key);
+        var endPos = template.indexOf("%}", startPos);
+        var borrar = template.substring(startPos, endPos + 2);
+        template = template.replace(borrar, "");
+
+        template = replaceText(template, key, object[key]);
+
+        startPos = template.indexOf("{%else%}");
+        endPos = template.indexOf("{%endif%}", startPos);
+        borrar = template.substring(startPos, endPos + 9);
+        template = template.replace(borrar, "");
+      } else {
+
+        var startPos = template.indexOf("{%if");
+        var endPos = template.indexOf("{%else%}");
+        var borrar = template.substring(startPos, endPos + 9);
+        template = template.replace(borrar, "");
+        template = template.replace("{%endif%}", "");
+      }
+      return template;
+    }
+
+    var ifSimpleTemplate = function(template, object) {
+      for(var key in object) {
+        template = ifSimple(template, object, key);
+      }
+      return template;   
+    }
+
+
+    /*    Render template     */
+
+    trabajos.forEach(function(trabajo) {
+      var template = document.getElementById("trabajosTemplate").innerHTML;
+      template = replaceTemplate(template, {
+        "empresa": trabajo.empresa,        
+        "empleo": trabajo.empleo,
+        "descripcion": trabajo.descripcion,
+        "lugar": trabajo.lugar,
+        "fecha": trabajo.fecha
+      });
+    
+
+      template = ifComplete(template, trabajo, "url");
+      template = replaceText(template, "empresa", trabajo.empresa);
+
+      var el = document.createElement('li');
+
+      el.innerHTML = template;
+
+      document.querySelector('#trabajos').appendChild(el);
+    });
+
+
+    formacion.forEach(function(titulo) {
+      var template = document.getElementById("titulosTemplate").innerHTML;
+      template = replaceTemplate(template, {
+        "nombre": titulo.nombre,
+        "entidad": titulo.entidad,
+        "fecha": titulo.fecha
+      });
+      template = ifSimple(template, titulo, "matricula");      
+
+      var el = document.createElement('li');
+
+      el.innerHTML = template;
+
+      document.querySelector('#titulacion').appendChild(el);
+    });
+
+    cursos.forEach(function(titulo) {
+      var template = document.getElementById("cursosTemplate").innerHTML;
+      template = replaceTemplate(template, {
+        "nombre": titulo.nombre,
+        "entidad": titulo.entidad,
+        "fecha": titulo.fecha
+      });
+      template = ifComplete(template, titulo, "url");
+      template = replaceText(template, "nombre", titulo.nombre);    
+
+      var el = document.createElement('li');
+
+      el.innerHTML = template;
+
+      document.querySelector('#cursos').appendChild(el);
+    });
+
+    proyectos.forEach(function(project) {
+      var template = document.getElementById("porfolioTemplate").innerHTML;
+      template = replaceTemplate(template, {
+        "url": project.url,
+        "img": project.img,
+        "alt": project.alt,
+        "nombre": project.nombre
+      });
+
+      template = ifSimpleTemplate(template, {
+        "git": project.git,
+        "doc": project.doc,
+        "slide": project.slide
+      });
+
+      var el = document.createElement('div');
+      addClass(el, "projecto");
+
+      el.innerHTML = template;
+
+      document.querySelector('#porfolio-content').appendChild(el);
+    });
+
+
+
+  }
+}
