167b2a7a134bed0995bf703d8421e49eea16ddd6
Salvador <salvacams@gmail.com>
2016-11-07 10:56:55 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'salvacamPersonalWeb-v1.1.1';
+var cacheName = 'salvacamPersonalWeb-v1.1';
 var filesToCache = [
   '/',
   'index.html',
@@ -37,22 +37,22 @@ var filesToCache = [
 ];
 
 self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install_');
+  console.log('[ServiceWorker] Install');
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell_');
+      console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
     })
   );
 });
 
 self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate_');
+  console.log('[ServiceWorker] Activate');
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
         if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache_', key);
+          console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
         }
       }));
