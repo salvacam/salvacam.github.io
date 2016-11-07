7955bb49aa5625c6d79f798db0d782ea4c39aac8
Salvador <salvacams@gmail.com>
2016-11-07 10:55:27 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'salvacamPersonalWeb-v1.1';
+var cacheName = 'salvacamPersonalWeb-v1.1.1';
 var filesToCache = [
   '/',
   'index.html',
@@ -37,22 +37,22 @@ var filesToCache = [
 ];
 
 self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+  console.log('[ServiceWorker] Install_');
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
+      console.log('[ServiceWorker] Caching app shell_');
       return cache.addAll(filesToCache);
     })
   );
 });
 
 self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
+  console.log('[ServiceWorker] Activate_');
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
         if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
+          console.log('[ServiceWorker] Removing old cache_', key);
           return caches.delete(key);
         }
       }));
@@ -61,7 +61,7 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
+  console.log('[ServiceWorker] Fetch_', e.request.url);
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
